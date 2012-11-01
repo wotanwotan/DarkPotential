b5b1422015376bb655379622eb8f6a9d3f9bd391
Joel Glanfield <joelglanfield@gmail.com>
2012-11-01 20:51:29 -0300

    native iOS project added

---
+++ DarkPotential/DarkPotential/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DarkPotential
+//
+//  Created by Joel Glanfield on 2012-11-01.
+//  Copyright (c) 2012 Joel Glanfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ DarkPotential/DarkPotentialTests/DarkPotentialTests.h
@@ -0,0 +1,13 @@
+//
+//  DarkPotentialTests.h
+//  DarkPotentialTests
+//
+//  Created by Joel Glanfield on 2012-11-01.
+//  Copyright (c) 2012 Joel Glanfield. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DarkPotentialTests : SenTestCase
+
+@end
